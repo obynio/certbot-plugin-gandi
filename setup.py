9c799c4522895d4ae71c18993874069fc60ccc14
Yohann Leon <yohann@leon.re>
2019-02-26 00:21:17 +0100

    pip: add details to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='certbot-plugin-gandi',
-    version='1.0.0',
+    version='1.0.1',
     author="Yohann Leon",
     author_email="yohann@leon.re",
     description="Certbot plugin for authentication using Gandi LiveDNS",
@@ -13,6 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/obynio/certbot-plugin-gandi",
     packages=find_packages(),
+    python_requires=' >=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
         'certbot',
         'zope.interface',
@@ -25,10 +26,23 @@ setup(
     },
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
         'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
         ],
 )
